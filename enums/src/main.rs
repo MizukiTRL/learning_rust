2d0654a27b60e7fcd42ea6ab99211c768094aa9c
MizukiTRL <natsumitrlwaifu@gmail.com>
2023-09-11 22:56:16 -0500

    Starting enums training

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,4 +1,27 @@
 fn main() {
-    println!("Hello, world!");
-    println!("meow world");
+    let four = IpAddrKind::V4(String::from("127.0.0.1"));
+    let six = IpAddrKind::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("Hola gato"));
+    m.call();
 }
+
+enum IpAddrKind {
+    V4(String),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message  {
+    fn call(&self){
+       println!("{:?}", self);
+    }
+}
+
