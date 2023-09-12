fa95a12e0c84f0a6f9846ebbc84bb0fecfe6bc57
MizukiTRL <natsumitrlwaifu@gmail.com>
2023-09-12 17:21:18 -0500

    midway through enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -4,6 +4,16 @@ fn main() {
 
     let m = Message::Write(String::from("Hola gato"));
     m.call();
+
+    let nums: [i32; 10] = Default::default();;
+
+    for i in nums{
+        println!("{i}");
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
 
 enum IpAddrKind {
@@ -27,6 +37,39 @@ impl Message  {
            Self::Move { x, y } => println!("moved"),
            Self::Quit => println!("exited"),
        }
+    }
+}
+
+enum UsState {
+    Alabama,
+    Alaska,
+    texas,
+    florida,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
